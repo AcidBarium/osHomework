0f58b83c3369a0418ac167102446b6b1f67291ba
Acidbarium <acidbarium@163.com>
2025-04-07 08:21:08 +0800

    增加了一份线程代码

---
+++ OSEuThird/cialloWorld.c
@@ -0,0 +1,25 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void *worker(void *arg)
+{
+    printf("Ciallo world!\n");
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t tid;
+
+    if (pthread_create(&tid, NULL, worker, NULL))
+    {
+        printf("can not create\n");
+        exit(1);
+    }
+
+    printf("main waiting for thread\n");
+
+    pthread_join(tid, NULL);
+
+    exit(0);
+}
