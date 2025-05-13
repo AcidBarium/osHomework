786c87db168e94caa73608f883543475c59a4dd8
Acidbarium <acidbarium@163.com>
2025-05-13 22:51:10 +0800

    Update barber.c

--- machineFirst/code/barber.c
+++ machineFirst/code/barber.c
@@ -5,20 +5,28 @@
 #include <semaphore.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-sem_t customer;  // 顾客数量（信号量）
-int customers_waiting = 0; // 等待的顾客数量
+sem_t customer;
+
+int customers_waiting = 0;
+int total_customers = 5;
 
 void* barber(void* arg) {
     while (1) {
-        sem_wait(&customer); // 等待顾客来叫醒
+        sem_wait(&customer); // 没顾客就阻塞
         pthread_mutex_lock(&mutex);
 
         customers_waiting--;
         printf("Barber: Cutting hair... Customers waiting: %d\n", customers_waiting);
+        total_customers--;
 
         pthread_mutex_unlock(&mutex);
 
-        sleep(rand() % 2 + 1); // 理发时间
+        sleep(rand() % 2 + 1);
+
+        if (total_customers == 0) {
+            printf("Barber: All customers done, going to sleep.\n");
+            break;
+        }
     }
     return NULL;
 }
@@ -32,28 +40,27 @@ void* client(void* arg) {
     pthread_mutex_unlock(&mutex);
 
     sem_post(&customer); // 通知理发师
-
     return NULL;
 }
 
 int main() {
-    pthread_t barber_tid, customer_tids[40];
+    pthread_t barber_tid, customer_tids[5];
     srand(time(0));
 
     sem_init(&customer, 0, 0);
 
     pthread_create(&barber_tid, NULL, barber, NULL);
 
-    for (int i = 0; i < 30; i++) {
-        sleep(rand() % 3); // 顾客随机到来
+    for (int i = 0; i < 5; i++) {
+        sleep(rand() % 3);
         pthread_create(&customer_tids[i], NULL, client, NULL);
     }
 
-    for (int i = 0; i < 30; i++) {
+    for (int i = 0; i < 5; i++) {
         pthread_join(customer_tids[i], NULL);
     }
 
-    // barber 是无限循环，不用 join
+    pthread_join(barber_tid, NULL);
 
     sem_destroy(&customer);
     return 0;
