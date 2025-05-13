770e3e81be29d4a6b6fe546e854c02519d051180
Acidbarium <acidbarium@163.com>
2025-05-13 19:43:17 +0800

    Update produceConsum.c

--- machineFirst/produceConsum.c
+++ machineFirst/produceConsum.c
@@ -2,26 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <semaphore.h>
 
-int sum = 0; 
-int max_sum = 10; 
+int sum = 0;  
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t not_full = PTHREAD_COND_INITIALIZER;
-pthread_cond_t not_empty = PTHREAD_COND_INITIALIZER; 
+sem_t not_empty;   
 
 void* producer(void* arg) {
     while (1) {
-        pthread_mutex_lock(&mutex);
 
-        while (sum >= max_sum) {
-            pthread_cond_wait(&not_full, &mutex);
-        }
-        sum++;
+        pthread_mutex_lock(&mutex);  
+        sum++;  
         printf("Producer produced an item. Current sum: %d\n", sum);
-        pthread_cond_signal(&not_empty);
-        pthread_mutex_unlock(&mutex);
-        sleep(rand() % 2 + 1);
+
+        sem_post(&not_empty);  
+        pthread_mutex_unlock(&mutex); 
+
+        sleep(rand() % 2 + 1);  
     }
 
     return NULL;
@@ -29,26 +28,40 @@ void* producer(void* arg) {
 
 void* consumer(void* arg) {
     while (1) {
-        pthread_mutex_lock(&mutex);
-        while (sum <= 0) {
-            pthread_cond_wait(&not_empty, &mutex);
-        }
-        sum--;
+        sem_wait(&not_empty);  
+
+        pthread_mutex_lock(&mutex); 
+
+        sum--;  
         printf("Consumer consumed an item. Current sum: %d\n", sum);
-        pthread_cond_signal(&not_full);
+
+
         pthread_mutex_unlock(&mutex);
-        sleep(rand() % 2 + 2);
+
+        sleep(rand() % 2 + 2);  
     }
+
     return NULL;
 }
 
 int main() {
     pthread_t producers[2], consumers[2];
+
+    srand(time(0));
+
+    sem_init(&not_empty, 0, 0);  
+
     for (int i = 0; i < 2; i++) {
         pthread_create(&producers[i], NULL, producer, NULL);
         pthread_create(&consumers[i], NULL, consumer, NULL);
     }
-    sleep(10);
+
+    for (int i = 0; i < 2; i++) {
+        pthread_join(producers[i], NULL);
+        pthread_join(consumers[i], NULL);
+    }
+
+    sem_destroy(&not_empty);
 
     return 0;
 }
