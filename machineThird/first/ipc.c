39280a9973c4e5cd5c5ba59e654eede71f3afa52
Acidbarium <acidbarium@163.com>
2025-05-27 18:58:22 +0800

    修改示例代码

--- machineThird/first/ipc.c
+++ machineThird/first/ipc.c
@@ -1,188 +1,188 @@
+/*
+	Filename: ipc.c
+	Copyright: (C) 2006 by zhonghonglie
+	Function: 实现 IPC 机制的核心函数
+*/
 #include "ipc.h"
 
-// 生产消费者共享缓冲区即其有关的变量
-key_t buff_key;
-int buff_num;
-char *buff_ptr;
+// 全局变量声明（需在ipc.c中定义）
+key_t buff_key;     // 缓冲区共享内存键值
+int buff_num;       // 缓冲区大小（字节数）
+char *buff_ptr;     // 缓冲区指针
 
 // 生产者放产品位置的共享指针
-key_t pput_key;
-int pput_num;
-int *pput_ptr;
+key_t pput_key;     // 生产者指针键值
+int pput_num;       // 指针数量
+int *pput_ptr;      // 生产者指针
 
 // 消费者取产品位置的共享指针
-key_t cget_key;
-int cget_num;
-int *cget_ptr;
-int buff_num;
-int pput_num;
-int buff_number;
-int pput_number;
-int cget_number;
+key_t cget_key;     // 消费者指针键值
+int cget_num;       // 指针数量
+int *cget_ptr;      // 消费者指针
 
-key_t pput_h;
-key_t prod_h;
-key_t pmtx_h;
-key_t cons_h;
-key_t cmtx_h;
+// 信号量相关全局变量
+key_t prod_key;     // 生产者同步信号量键值
+key_t pmtx_key;     // 生产者互斥信号量键值
+int prod_sem;       // 生产者同步信号量ID
+int pmtx_sem;       // 生产者互斥信号量ID
 
-key_t buff_h;
-key_t cget_h;
+key_t cons_key;     // 消费者同步信号量键值
+key_t cmtx_key;     // 消费者互斥信号量键值
+int cons_sem;       // 消费者同步信号量ID
+int cmtx_sem;       // 消费者互斥信号量ID
 
-// 生产者有关的信号量
-key_t prod_key;
-key_t pmtx_key;
-int prod_sem;
-int pmtx_sem;
+int sem_val;        // 信号量初始值
+int sem_flg;        // 信号量操作标志
+int shm_flg;        // 共享内存操作标志
 
-// 消费者有关的信号量
-key_t cons_key;
-key_t cmtx_key;
-int cons_sem;
-int cmtx_sem;
-int sem_val;
-int sem_flg;
-int shm_flg;
-int get_ipc_id(char *proc_file, key_t key)
-{
-    FILE *pf;                       // 定义文件指针
-    int i, j;                       // 定义整型变量i和j
-    char line[BUFSZ], colum[BUFSZ]; // 定义字符数组line和colum
-    if ((pf = fopen(proc_file, "r")) == NULL)
-    {                                 // 如果文件指针为空，则出错
-        perror("Proc file not open"); // 打印错误信息
-        exit(EXIT_FAILURE);           // 退出程序
-    }
-    fgets(line, BUFSZ, pf); // 读取文件的第一行
-    while (!feof(pf))
-    {                               // 如果文件没有结束
-        i = j = 0;                  // 初始化i和j
-        fgets(line, BUFSZ, pf);     // 读取文件的一行
-        while (line[i] == ' ')      // 如果这一行是空格
-            i++;                    // 则跳过这一行
-        while (line[i] != ' ')      // 如果这一行不是空格
-            colum[j++] = line[i++]; // 则将这一行的内容赋值给colum
-        colum[j] = '\0';            // 将colum字符数组的最后一个元素赋值为空字符
-        if (atoi(colum) != key)     // 如果atoi(colum)的值不等于key
-            continue;               // 则跳过这一行
-        j = 0;                      // 初始化j
-        while (line[i] == ' ')      // 如果这一行是空格
-            i++;                    // 则跳过这一行
-        while (line[i] != ' ')      // 如果这一行不是空格
-            colum[j++] = line[i++]; // 则将这一行的内容赋值给colum
-        colum[j] = '\0';            // 将colum字符数组的最后一个元素赋值为空字符
-        i = atoi(colum);            // 将colum字符数组的值转换为整数
-        fclose(pf);                 // 关闭文件
-        return i;                   // 返回整数值
-    }
-    fclose(pf); // 关闭文件
-    return -1;  // 返回-1
-}
-int down(int sem_id)
-{
-    // 声明一个struct sembuf类型的变量buf
-    struct sembuf buf;
-    // 将buf.sem_op设置为-1
-    buf.sem_op = -1;
-    // 将buf.sem_num设置为0
-    buf.sem_num = 0;
-    // 将buf.sem_flg设置为SEM_UNDO
-    buf.sem_flg = SEM_UNDO;
-    // 如果semop函数的返回值小于0，则输出错误信息，并退出程序
-    if ((semop(sem_id, &buf, 1)) < 0)
-    {
-        perror("down error ");
-        exit(EXIT_FAILURE);
-    }
-    // 返回成功
-    return EXIT_SUCCESS;
+/*
+	get_ipc_id() - 从/proc/sysvipc/文件系统中获取IPC对象的ID
+	@proc_file: 对应的IPC文件路径（msg/sem/shm）
+	@key: 要获取的IPC对象键值
+*/
+int get_ipc_id(char *proc_file, key_t key) {
+	FILE *pf;
+	int i, j;
+	char line[BUFSZ], colum[BUFSZ];
+
+	if ((pf = fopen(proc_file, "r")) == NULL) {
+		perror("Proc file not open");
+		exit(EXIT_FAILURE);
+	}
+	fgets(line, BUFSZ, pf); // 跳过首行标题
+
+	while (!feof(pf)) {
+		i = j = 0;
+		fgets(line, BUFSZ, pf); // 读取数据行
+
+		// 解析键值字段
+		while (line[i] == ' ') i++;
+		while (line[i] != ' ') colum[j++] = line[i++];
+		colum[j] = '\0';
+
+		if (atoi(colum) != key) continue; // 键值匹配检测
+
+		// 提取IPC ID
+		j = 0;
+		while (line[i] == ' ') i++;
+		while (line[i] != ' ') colum[j++] = line[i++];
+		colum[j] = '\0';
+		fclose(pf);
+		return atoi(colum);
+	}
+	fclose(pf);
+	return -1; // 未找到对应IPC对象
 }
 
-int up(int sem_id)
-{
-    struct sembuf buf;
-    buf.sem_op = 1;
-    buf.sem_num = 0;
-    buf.sem_flg = SEM_UNDO;
-    if ((semop(sem_id, &buf, 1)) < 0)
-    {
-        perror("up error ");
-        exit(EXIT_FAILURE);
-    }
-    return EXIT_SUCCESS;
+/*
+	down() - 信号量P操作（阻塞）
+	@sem_id: 信号量ID
+*/
+int down(int sem_id) {
+	struct sembuf buf;
+	buf.sem_op = -1;       // 减1操作
+	buf.sem_num = 0;       // 信号量数组下标
+	buf.sem_flg = SEM_UNDO; // 自动恢复机制
+
+	if ((semop(sem_id, &buf, 1)) < 0) {
+		perror("down error");
+		exit(EXIT_FAILURE);
+	}
+	return EXIT_SUCCESS;
 }
 
-int set_sem(key_t sem_key, int sem_val, int sem_flg)
-{
-    int sem_id;
-    Sem_uns sem_arg;
+/*
+	up() - 信号量V操作（释放）
+	@sem_id: 信号量ID
+*/
+int up(int sem_id) {
+	struct sembuf buf;
+	buf.sem_op = 1;       // 加1操作
+	buf.sem_num = 0;      // 信号量数组下标
+	buf.sem_flg = SEM_UNDO; // 自动恢复机制
 
-    // 测试由 sem_key 标识的信号灯数组是否已经建立
-    if ((sem_id = get_ipc_id("/proc/sysvipc/sem", sem_key)) < 0)
-    {
-        // semget 新建一个信号灯,其标号返回到 sem_id
-        if ((sem_id = semget(sem_key, 1, sem_flg)) < 0)
-        {
-            perror("semaphore create error");
-            exit(EXIT_FAILURE);
-        }
-        // 设置信号灯的初值
-        sem_arg.val = sem_val;
-        if (semctl(sem_id, 0, SETVAL, sem_arg) < 0)
-        {
-            perror("semaphore set error");
-            exit(EXIT_FAILURE);
-        }
-    }
-    return sem_id;
+	if ((semop(sem_id, &buf, 1)) < 0) {
+		perror("up error");
+		exit(EXIT_FAILURE);
+	}
+	return EXIT_SUCCESS;
 }
 
-char *set_shm(key_t shm_key, int shm_num, int shm_flg)
-{
-    int i, shm_id;
-    char *shm_buf;
+/*
+	set_sem() - 创建/获取信号量并初始化
+	@sem_key: 信号量键值
+	@sem_val: 初始值
+	@sem_flg: 权限标志
+*/
+int set_sem(key_t sem_key, int sem_val, int sem_flg) {
+	int sem_id;
+	Sem_uns sem_arg;
 
-    // 测试由 shm_key 标识的共享内存区是否已经建立
-    if ((shm_id = get_ipc_id("/proc/sysvipc/shm", shm_key)) < 0)
-    {
-        // shmget 新建 一个长度为 shm_num 字节的共享内存,其标号返回到 shm_id
-        if ((shm_id = shmget(shm_key, shm_num, shm_flg)) < 0)
-        {
-            perror("shareMemory set error");
-            exit(EXIT_FAILURE);
-        }
-        // shmat 将由 shm_id 标识的共享内存附加给指针 shm_buf
-        if ((shm_buf = (char *)shmat(shm_id, 0, 0)) < (char *)0)
-        {
-            perror("get shareMemory error");
-            exit(EXIT_FAILURE);
-        }
-        for (i = 0; i < shm_num; i++)
-            shm_buf[i] = 0;
-        // 初始为 0
-    }
-    // shm_key 标识的共享内存区已经建立,将由 shm_id 标识的共享内存附加给指 针
-    // shm_buf
-    if ((shm_buf = (char *)shmat(shm_id, 0, 0)) < (char *)0)
-    {
-        perror("get shareMemory error");
-        exit(EXIT_FAILURE);
-    }
-    return shm_buf;
+	// 检查信号量是否存在
+	if ((sem_id = get_ipc_id("/proc/sysvipc/sem", sem_key)) < 0) {
+		// 创建新信号量（1个元素）
+		if ((sem_id = semget(sem_key, 1, sem_flg)) < 0) {
+			perror("semaphore create error");
+			exit(EXIT_FAILURE);
+		}
+		// 初始化信号量值
+		sem_arg.val = sem_val;
+		if (semctl(sem_id, 0, SETVAL, sem_arg) < 0) {
+			perror("semaphore set error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return sem_id;
 }
 
-int set_msq(key_t msq_key, int msq_flg)
-{
-    int msq_id;
-    // 测试由 msq_key 标识的消息队列是否已经建立
-    if ((msq_id = get_ipc_id("/proc/sysvipc/msg", msq_key)) < 0)
-    {
-        // msgget 新建一个消息队列,其标号返回到 msq_id
-        if ((msq_id = msgget(msq_key, msq_flg)) < 0)
-        {
-            perror("messageQueue set error");
-            exit(EXIT_FAILURE);
-        }
-    }
-    return msq_id;
+/*
+	set_shm() - 创建/获取共享内存并附加
+	@shm_key: 共享内存键值
+	@shm_num: 内存大小（字节）
+	@shm_flg: 权限标志
+*/
+char *set_shm(key_t shm_key, int shm_num, int shm_flg) {
+	int i, shm_id;
+	char *shm_buf;
+
+	// 检查共享内存是否存在
+	if ((shm_id = get_ipc_id("/proc/sysvipc/shm", shm_key)) < 0) {
+		// 创建新共享内存
+		if ((shm_id = shmget(shm_key, shm_num, shm_flg)) < 0) {
+			perror("shareMemory set error");
+			exit(EXIT_FAILURE);
+		}
+		// 附加到进程地址空间
+		if ((shm_buf = (char *)shmat(shm_id, 0, 0)) < (char *)0) {
+			perror("get shareMemory error");
+			exit(EXIT_FAILURE);
+		}
+		for (i = 0; i < shm_num; i++) shm_buf[i] = 0; // 初始化为0
+	} else {
+		// 附加已有内存
+		if ((shm_buf = (char *)shmat(shm_id, 0, 0)) < (char *)0) {
+			perror("get shareMemory error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return shm_buf;
+}
+
+/*
+	set_msq() - 创建/获取消息队列
+	@msq_key: 队列键值
+	@msq_flg: 权限标志
+*/
+int set_msq(key_t msq_key, int msq_flg) {
+	int msq_id;
+
+	// 检查消息队列是否存在
+	if ((msq_id = get_ipc_id("/proc/sysvipc/msg", msq_key)) < 0) {
+		// 创建新队列
+		if ((msq_id = msgget(msq_key, msq_flg)) < 0) {
+			perror("messageQueue set error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return msq_id;
 }
--- machineThird/first/producer.c
+++ machineThird/first/producer.c
@@ -1,63 +1,63 @@
 /*
- * Filename : producer.c
- * copyright : (C) 2006 by zhonghonglie
- * Function : 建立并模拟生产者进程
- */
+	Filename: producer.c
+	Copyright: (C) 2006 by zhonghonglie
+	Function: 模拟生产者进程，向共享缓冲区写入数据
+*/
 #include "ipc.h"
-int main(int argc, char *argv[])
-{
-    int rate;
-    // 可在在命令行第一参数指定一个进程睡眠秒数，以调解进程执行速度
-    if (argv[1] != NULL)
-        rate = atoi(argv[1]);
-    else
-        rate = 3;               // 不指定为 3 秒
-                                // 共享内存使用的变量buff_key = 101;//缓冲区任给的键值
-    buff_num = 8;               // 缓冲区任给的长度
-    pput_key = 102;             // 生产者放产品指针的键值
-    pput_num = 1;               // 指针数
-    shm_flg = IPC_CREAT | 0644; // 共享内存读写权限
-    // 获取缓冲区使用的共享内存，buff_ptr 指向缓冲区首地址
-    buff_ptr = (char *)set_shm(buff_key, buff_num, shm_flg);
-    // 获取生产者放产品位置指针 pput_ptr
-    pput_ptr = (int *)set_shm(pput_key, pput_num, shm_flg);
+#include <unistd.h>
 
-    // 信号量使用的变量
-    prod_key = 201; // 生产者同步信号灯键值
-    pmtx_key = 202; // 生产者互斥信号灯键值
-    cons_key = 301; // 消费者同步信号灯键值
-    cmtx_key = 302; // 消费者互斥信号灯键值
-    sem_flg = IPC_CREAT | 0644;
-    // 生产者同步信号灯初值设为缓冲区最大可用量
-    sem_val = buff_num;
-    // 获取生产者同步信号灯，引用标识存 prod_sem
-    prod_sem = set_sem(prod_key, sem_val, sem_flg);
-    // 消费者初始无产品可取，同步信号灯初值设为 0
-    sem_val = 0;
-    // 获取消费者同步信号灯，引用标识存 cons_sem
-    cons_sem = set_sem(cons_key, sem_val, sem_flg);
-    // 生产者互斥信号灯初值为 1
-    sem_val = 1;
-    // 获取生产者互斥信号灯，引用标识存 pmtx_sem
-    pmtx_sem = set_sem(pmtx_key, sem_val, sem_flg);
-    // 循环执行模拟生产者不断放产品
-    while (1)
-    {
-        // 如果缓冲区满则生产者阻塞
-        down(prod_sem);
-        // 如果另一生产者正在放产品，本生产者阻塞
-        down(pmtx_sem);
-        // 用写一字符的形式模拟生产者放产品，报告本进程号和放入的字符及存放的位置
-        buff_ptr[*pput_ptr] = 'A' + *pput_ptr;
-        sleep(rate);
-        printf("%d producer put: %c to Buffer[%d]\n", getpid(),
-               buff_ptr[*pput_ptr], *pput_ptr);
-        // 存放位置循环下移
-        *pput_ptr = (*pput_ptr + 1) % buff_num;
-        // 唤醒阻塞的生产者
-        up(pmtx_sem);
-        // 唤醒阻塞的消费者
-        up(cons_sem);
-    }
-    return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+	int rate;
+	// 通过命令行参数设置生产速率（默认3秒）
+	if (argv[1] != NULL)
+		rate = atoi(argv[1]);
+	else
+		rate = 3;
+
+	// 共享内存相关配置
+	buff_key = 101;     // 缓冲区共享内存键值
+	buff_num = 8;       // 缓冲区大小（字节数）
+	pput_key = 102;     // 生产者指针键值
+	pput_num = 1;       // 指针数量
+	shm_flg = IPC_CREAT | 0644; // 共享内存权限（读写）
+
+	// 获取/创建共享内存（缓冲区和指针）
+	buff_ptr = (char *)set_shm(buff_key, buff_num, shm_flg); // 缓冲区首地址
+	pput_ptr = (int *)set_shm(pput_key, pput_num, shm_flg);  // 写指针地址
+
+	// 信号量相关配置
+	prod_key = 201;     // 生产者同步信号量键值
+	pmtx_key = 202;     // 生产者互斥信号量键值
+	cons_key = 301;     // 消费者同步信号量键值
+	cmtx_key = 302;     // 消费者互斥信号量键值
+	sem_flg = IPC_CREAT | 0644; // 信号量权限（读写）
+
+	// 初始化信号量
+	sem_val = buff_num; // 生产者同步信号量初值=缓冲区大小（可用空槽位数）
+	prod_sem = set_sem(prod_key, sem_val, sem_flg); // 创建生产者同步信号量
+
+	sem_val = 0;        // 消费者同步信号量初值=0（初始无产品可取）
+	cons_sem = set_sem(cons_key, sem_val, sem_flg); // 创建消费者同步信号量
+
+	sem_val = 1;        // 互斥信号量初值=1（保护缓冲区访问）
+	pmtx_sem = set_sem(pmtx_key, sem_val, sem_flg); // 创建生产者互斥信号量
+
+	// 持续生产数据
+	while (1) {
+		down(prod_sem);  // 等待空槽位（P操作）
+		down(pmtx_sem);  // 进入临界区（P操作）
+
+		// 模拟生产数据：写入字符（A+位置索引）
+		buff_ptr[*pput_ptr] = 'A' + (*pput_ptr % 26); // 防止溢出ASCII范围
+		sleep(rate); // 模拟生产耗时
+		printf("%d producer put: %c to Buffer[%d]\n", getpid(), buff_ptr[*pput_ptr], *pput_ptr);
+
+		// 更新写指针位置（循环缓冲区）
+		*pput_ptr = (*pput_ptr + 1) % buff_num;
+
+		up(pmtx_sem);  // 离开临界区（V操作）
+		up(cons_sem);  // 通知消费者有新产品（V操作）
+	}
+
+	return EXIT_SUCCESS;
 }
