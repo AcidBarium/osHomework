e85696db6041c3dd61a5974b3f94b5dbab6970c8
Acidbarium <acidbarium@163.com>
2025-05-27 18:39:31 +0800

    Update consumer.c

--- machineThird/second/consumer.c
+++ machineThird/second/consumer.c
@@ -1,5 +1,6 @@
 #include "ipc.h"
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     int rate = 3;
     int consumerid = atoi(argv[1]);
     buff_h = 101;
@@ -7,8 +8,8 @@ int main(int argc, char* argv[]) {
     cget_h = 103;
     cget_number = 1;
     shm_flg = IPC_CREAT | 0644;
-    buff_ptr = (char*)set_shm(buff_h, buff_number, shm_flg);
-    cget_ptr = (int*)set_shm(cget_h, cget_number, shm_flg);
+    buff_ptr = (char *)set_shm(buff_h, buff_number, shm_flg);
+    cget_ptr = (int *)set_shm(cget_h, cget_number, shm_flg);
     prod_h = 201;
     pmtx_h = 202;
     cons_h = 301;
@@ -22,24 +23,29 @@ int main(int argc, char* argv[]) {
     cmtx_sem = set_sem(cmtx_h, sem_val, sem_flg);
     if (consumerid == 0)
         *cget_ptr = 0;
-    while (1) {
-        if (buff_ptr[0] - 'A' == consumerid) {
+    while (1)
+    {
+        if (buff_ptr[0] - 'A' == consumerid)
+        {
             down(cons_sem);
             down(cmtx_sem);
             sleep(rate);
-            if (buff_ptr[0] == 'A') {
+            if (buff_ptr[0] == 'A')
+            {
                 printf(
                     "%d The consumer has glue.\nThe consumer gets tobacco and "
                     "paper\n",
                     getpid());
             }
-            if (buff_ptr[0] == 'B') {
+            if (buff_ptr[0] == 'B')
+            {
                 printf(
                     "%d The consumer has paper.\nThe consumer gets tobacco and "
                     "glue\n",
                     getpid());
             }
-            if (buff_ptr[0] == 'C') {
+            if (buff_ptr[0] == 'C')
+            {
                 printf(
                     "%d The consumer has tobacco.\nThe consumer gets glue and "
                     "paper\n",
